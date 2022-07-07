51e204ccb10ac297e264a214367a3a9ccbea9740
Arjo <ac.ala.arya@gmail.com>
2022-07-07 11:08:44 +0530

    data for absolute separability

---
+++ src/data/main.py
@@ -0,0 +1,8 @@
+"""
+How to generate absolutely separable matrices with very high confidence?
+- Generate random 1e5 unitary matrices
+- Generate a random density matrix
+- If it stays separable after going through each random unitary transformation,
+it is absolutely separable.
+"""
+
---
+++ src/data/sep-classifier.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers
+import time
+
+from IPython import display
